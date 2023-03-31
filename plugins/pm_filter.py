ad0e7839da64b887c9e25f913f5ee2d993fa11ed
MYFLiiX <120738938+MYFLiiX@users.noreply.github.com>
2023-04-01 04:37:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -113,11 +113,7 @@ async def pm_text(bot, message):
     user = message.from_user.first_name
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
-    await message.reply_text("<b>MOVIES GROUP - @MYFLiiX_2
-
-🔞 SEXI VIDEOS - https://t.me/+G_NK1KOuNQAwY2I1
-
-JOIN BACKUP - @MYFLiiX</b>")
+    await message.reply_text("<b>Hello join here for all @MYFLiiX_2 !</b>")
     await bot.send_message(
         chat_id=LOG_CHANNEL,
         text=f"<b>#PM_MSG\n\nName : {user}\n\nID : {user_id}\n\nMessage : {content}</b>"
