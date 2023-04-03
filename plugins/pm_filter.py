e4c7a639307e672f92ae83b4687290c4e76687ff
MYFLiiX <120738938+MYFLiiX@users.noreply.github.com>
2023-04-04 03:42:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -743,7 +743,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
              InlineKeyboardButton('ᴄᴀʀʙᴏɴ', callback_data='carb'),
         ],  [
              InlineKeyboardButton('ᴄᴏᴜɴᴛʀʏ', callback_data='country'),
-             InlineKeyboardButton('ᴅᴇᴘʟᴏʏ', callback_data='deploy'),
+             InlineKeyboardButton('ʏᴛᴛʜᴜᴍʙ', callback_data='ytthumb'),
              InlineKeyboardButton('ᴡʀɪᴛᴇ', callback_data='write')
         ], [
              InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='aswin'),
