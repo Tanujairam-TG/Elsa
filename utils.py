d65edb03e2bf70d3c67be4c99f535ac4e923f501
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2023-04-08 23:21:12 +0530

    ADD [SEND ALL]

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import logging
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
-from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM, SHORTLINK_URL, SHORTLINK_API
+from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM, SHORTLINK_URL, SHORTLINK_API, MAIN_CHANNEL, CUSTOM_FILE_CAPTION
 from imdb import Cinemagoer
 import asyncio
 from pyrogram.types import Message, InlineKeyboardButton
@@ -484,3 +484,33 @@ async def get_shortlink(chat_id, link):
         except Exception as e:
             logger.error(e)
             return f'https://{URL}/api?api={API}&link={link}'
+
+
+async def send_all(bot, userid, files, ident):
+    for file in files:
+        f_caption = file.caption
+        title = file.file_name
+        size = get_size(file.file_size)
+        if CUSTOM_FILE_CAPTION:
+            try:
+                f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
+                                                        file_size='' if size is None else size,
+                                                        file_caption='' if f_caption is None else f_caption)
+            except Exception as e:
+                print(e)
+                f_caption = f_caption
+        if f_caption is None:
+            f_caption = f"{title}"
+        await bot.send_cached_media(
+            chat_id=userid,
+            file_id=file.file_id,
+            caption=f_caption,
+            protect_content=True if ident == "filep" else False,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                   [
+                     InlineKeyboardButton('Cʜᴀɴɴᴇʟ', url=MAIN_CHANNEL)
+                   ]
+                ]
+            )
+        )
