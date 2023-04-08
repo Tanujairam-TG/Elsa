4f78163215f819c866dce419a8c52b8666a1b1f2
MYFLiiX <120738938+MYFLiiX@users.noreply.github.com>
2023-04-09 03:39:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -205,7 +205,7 @@ async def next_page(bot, query):
     )
     btn.insert(2, 
          [
-             InlineKeyboardButton(f'ɪɴꜰᴏ', 'reqinfo'),
+             InlineKeyboardButton(f'🔞 ᴘᴏʀɴ 🔞', 'reqinfo'),
              InlineKeyboardButton(f'ᴍᴏᴠɪᴇ', 'minfo'),
              InlineKeyboardButton(f'sᴇʀɪᴇs', 'sinfo'),
              InlineKeyboardButton(f'ᴛɪᴘs', 'tinfo'),
@@ -1250,7 +1250,7 @@ async def auto_filter(client, msg, spoll=False):
     )
     btn.insert(2, 
          [
-             InlineKeyboardButton(f'ɪɴꜰᴏ', 'reqinfo'),
+             InlineKeyboardButton(f'🔞 ᴘᴏʀɴ 🔞', 'reqinfo'),
              InlineKeyboardButton(f'ᴍᴏᴠɪᴇ', 'minfo'),
              InlineKeyboardButton(f'sᴇʀɪᴇs', 'sinfo'),
              InlineKeyboardButton(f'ᴛɪᴘs', 'tinfo')
