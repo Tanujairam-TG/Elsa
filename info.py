ce5466ab1411e867e5ef86bb7f2647b7f93a934c
MYFLiiX <120738938+MYFLiiX@users.noreply.github.com>
2023-05-04 14:12:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ class evamaria(Client):
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
-API_HASH = environ['
+API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
